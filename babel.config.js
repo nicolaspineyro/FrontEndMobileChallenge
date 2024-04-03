791a9e15fdd28349c0ca2e6d02e0b1320c10df1d
nicolaspineyro <nicolaspineyro1999@gmail.com>
2024-04-02 22:25:58 -0300

    002: set typsecript, babel and installed dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          navigation: './src/navigation',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
